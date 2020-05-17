a7e5492c062797939c60ac52ea244e2f196c30bb
mugitea <mugitea7@gmail.com>
2020-05-17 19:55:27 +0900

    テストコミット

--- Assets/MessageManager/Script/Message.cs
+++ Assets/MessageManager/Script/Message.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class Message : MonoBehaviour
 {
 
+    [SerializeField] private bool mIsAbleToTalk = false;
+
+    //TODO: structにして、立ち絵を追加.
     [SerializeField, TextArea(1, 10)] private List<string> mMessage = new List<string> { "ruhgurhgurhgurhg", "ijitjhtkfskdjfg" };
+
+    public bool IsAbleToTalk
+    {
+        get
+        {
+            return mIsAbleToTalk;
+        }
+    }
 
     public List<string> GetMessage {
         get
